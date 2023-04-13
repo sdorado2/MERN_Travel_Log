29d1141d4c7b6566d2f0ac02a3cf7256a4e32c28
sdorado2 <sdorado2@gmail.com>
2023-04-13 10:10:59 -0400

    added jsx engine and modified defaultlayout

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+// app.get("/", (req, res) => {
+//   res.send(`Testing`);
+// });
+
 app.get("/", (req, res) => {
-  res.send(`Testing`);
+  res.render("Index");
 });
 
 app.listen(PORT, () => console.log(`Listening @ ${PORT}`));
--- views/layout/Default.jsx
+++ views/layout/Default.jsx
@@ -5,15 +5,14 @@ class DefaultLayout extends React.Component {
     return (
       <html lang="en">
         <head>
-          <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
           <title>Document</title>
         </head>
-        <body></body>
+        <body>{this.props.children}</body>
       </html>
     );
   }
