a7e0d52ab2d56a27efad7b85f61ee86b3ccd9ef6
sdorado2 <sdorado2@gmail.com>
2023-04-13 11:14:13 -0400

    added a form to New page

--- views/New.jsx
+++ views/New.jsx
@@ -6,6 +6,19 @@ class New extends React.Component {
     return (
       <DefaultLayout>
         <h1>New Page</h1>
+
+        <form>
+          Image URL : <input type="url" />
+          <br />
+          Title : <input type="text" required={true} />
+          <br />
+          Date : <input type="date" required={true} />
+          <br />
+          <p>Summary : </p>
+          <textarea maxLength="240" minLength="10" required={true}></textarea>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </DefaultLayout>
     );
   }
