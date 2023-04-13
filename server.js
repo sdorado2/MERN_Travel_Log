6c0c14cae70bcef7616eaa0e59b5b8f6df16f7fe
sdorado2 <sdorado2@gmail.com>
2023-04-13 09:16:14 -0400

    Testing to listening port successful

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send(`Testing`);
+});
+
 app.listen(PORT, () => console.log(`Listening @ ${PORT}`));
