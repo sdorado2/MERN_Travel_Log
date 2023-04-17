e7719e5e6c58b2b199b972007970db766ba86ad1
sdorado2 <sdorado2@gmail.com>
2023-04-17 10:58:44 -0400

    added a location model

---
+++ models/location.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+  latitude: { type: String },
+  longitude: { type: String },
+  city: { type: String },
+  country: { type: String },
+});
+
+const Location = mongoose.model("Location", locationSchema);
+
+module.export = Location;
