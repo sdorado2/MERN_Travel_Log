77b74463ba1e35cf3be0975eeaf6188bb30017ac
sdorado2 <sdorado2@gmail.com>
2023-04-18 23:41:34 -0400

    added css to h1 in Index page

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,7 +6,7 @@ class Index extends React.Component {
     const travel = this.props.log;
     return (
       <DefaultLayout>
-        <h1>Index Page</h1>
+        <h1 className="index">Index Page</h1>
         <a href="/new">Add New</a>
         {travel.map((entry) => {
           return (
