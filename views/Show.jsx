5d100681305457f988d4f5b9b558339d76751eb3
sdorado2 <sdorado2@gmail.com>
2023-04-19 13:11:08 -0400

    added css to show page

--- views/Show.jsx
+++ views/Show.jsx
@@ -8,19 +8,23 @@ class Show extends React.Component {
     console.log(`Show GEO.Country :${log.geo[0].country}`);
     return (
       <DefaultLayout title={log.title}>
-        <h1>{log.title}</h1>
-        <img src={log.img} alt="image" className="logImg" />
+        <div className="show">
+          <h1>{log.title}</h1>
+          <img src={log.img} alt="image" className="logImg" />
 
-        <h3>
-          Location : {log.geo[0].city}, {log.geo[0].country}
-        </h3>
-        <p>{log.summary}</p>
-        <a href="/">
-          <button>Back</button>
-        </a>
-        <form action={`/${log._id}?_method=delete`} method="POST">
-          <input type="submit" value="delete" />
-        </form>
+          <h3>
+            Location : {log.geo[0].city}, {log.geo[0].country}
+          </h3>
+          <p>{log.summary}</p>
+        </div>
+        <div className="showOptions">
+          <a href="/">
+            <button>Back</button>
+          </a>
+          <form action={`/${log._id}?_method=delete`} method="POST">
+            <input type="submit" value="delete" />
+          </form>
+        </div>
       </DefaultLayout>
     );
   }
