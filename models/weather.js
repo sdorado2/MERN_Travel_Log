b7afd611626211d6a53fea802546195e87846a09
sdorado2 <sdorado2@gmail.com>
2023-04-19 21:25:40 -0400

    added timestamp to weatherSchema

--- models/weather.js
+++ models/weather.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const weatherSchema = new mongoose.Schema({
-  time: { type: String },
-  weathercode: { type: String },
-  temperatureMax: { type: String },
-  temperatureMin: { type: String },
-});
+const weatherSchema = new mongoose.Schema(
+  {
+    date: { type: String },
+    weathercode: { type: String },
+    temperatureMax: { type: String },
+    temperatureMin: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Weather = mongoose.model("Weather", weatherSchema);
 
